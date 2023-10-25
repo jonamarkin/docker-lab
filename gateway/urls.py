b82fb1392f9a1111732bc3e9590ca60bfd6937bf
Jonathan Ato Markin <jona10711@gmail.com>
2023-10-25 17:44:20 +0200

    Docker compose file completed and project completed

--- gateway/urls.py
+++ gateway/urls.py
@@ -1,3 +1,3 @@
-GATEWAY_URL = 'http://GATEWAY_URL:' #also add gateway service port
-MATH_URL = 'http://MATH_URL:' #also add math service port
-STRING_URL = 'http://STRING_URL:' #also add string service port
+GATEWAY_URL = 'http://gateway:8082' #also add gateway service port
+MATH_URL = 'http://math-service:8080' #also add math service port
+STRING_URL = 'http://string-service:8081' #also add string service port
